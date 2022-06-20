f00aa9621fc97f3137c750e90dd50ee993597620
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-06-20 14:46:30 -0600

    Tokenizer functions completed

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
 
@@ -37,23 +38,25 @@ char *word_terminator(char *word)
 int count_words(char *str)
 {
   int n_words = 0;
-  do {
-    str = word_start(str);
-    if (*str != '\0')
-      n_words++;
-    str = word_terminator(str);
-  } while (*str != '\0');
-  return n_words;
+  while (*str != '\0') {
+    str = word_start(str);  //Get to the beginning of the next word
+    if (*str != '\0') //If the next word start with a '\0' is because there are no more words
+      n_words++;      //Add one to the counter
+    str = word_terminator(str); //Get to the end of the word
+  }
+  return n_words; //Return number of words
 }
 
 /* Returns a freshly allocated new zero-terminated string containing <len> chars from <inStr> */
 char *copy_str(char *inStr, short len)
 {
-  char *new_str = malloc(sizeof(char) * len);
+  char *new_str = malloc(sizeof(char) * len); //Allocate enough space to store len characters
   if (new_str == NULL) return NULL; //return null if there was not enough space in malloc
-  char *temp = new_str;
-  while (*temp++ = *inStr++);
-  return new_str;
+  char *temp = new_str;   //Create a pointer to new_str
+  while (len-- > 0)       //While we have more character to copy
+    (*temp++ = *inStr++); //Copy next character and increase temp and inStr by one
+  *(--temp) = '\0';       //Change the last character to a '\0'
+  return new_str;         //Return the copy word
 }
 
 /* Returns a freshly allocated zero-terminated vector of freshly allocated 
@@ -62,18 +65,34 @@ char *copy_str(char *inStr, short len)
     tokens[0] = "hello"     tokens[1] = "world"     tokens[2] = "string"     tokens[3] = 0 */
 char **tokenize(char *str)
 {
-  return 0;
+  int n_words = count_words(str); //Calculate number of words in str
+  char **arr = malloc(sizeof(char) * (n_words+1));  //Allocate enough space to store (n_words+1) pointers to characters
+  char *start_word, *end_word;  //Make pointer for the beginning and end of word
+
+  for (int i = 0; i < n_words; i++) {
+    start_word = word_start(str);     //Get beginning of word
+    end_word = word_terminator(str);  //Get end of word
+    arr[i] = copy_str(start_word,end_word-start_word+1);  //Copy word
+    str = end_word + 1; //Start after the blank of the found word
+  }
+  arr[n_words] = '\0';  //Set last item in arr to '\0'
+  return arr; //Return array of pointer to arrays of characters
 }
 
 /* Prints all tokens. */
 void print_tokens(char **tokens)
 {
+  for (char **p = tokens; *p; p++)  //Iterate every token in tokens
+    printf("%s ",*p);   //Print string being pointed by p
+  printf("\n");
   return;
 }
 
-/* Frees all tokens and the vector containing themx. */
+/* Frees all tokens and the vector containing them. */
 void free_tokens(char **tokens)
 {
+  for (char **p = tokens; *p; p++)  //Iterate every token in tokens
+    free(p);    //Free the space allocated for the string in p
   return;
 }
 
--- src/uimain.c
+++ src/uimain.c
@@ -10,12 +10,19 @@ int main(int argc, char **argv)
   printf("\n");
 
   char *str = "A b C?";
+
   //count_words test
-  printf("%d == 3?\n", count_words(str));
+  int n_words = count_words(str);
+  printf("%d == 3?\n", n_words);
 
   printf("str1 = %s,\t str1 address: %x\n",str,str);
   char *str2 = copy_str(str,7);
-  printf("str2 = %s\t, str2 address: %x\n ",str2, str2);
+  printf("str2 = %s\t, str2 address: %x\n",str2, str2);
+
+  //Tokenize
+  char **arr = tokenize(str);
+  printf("Tokenize function finished.\n");
+  print_tokens(arr);
   return 0;
 }
 
